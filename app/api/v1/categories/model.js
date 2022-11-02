53ef835891d711765b8052e28b9c603f3438e7ca
zakihaha <muhzakich@students.amikom.ac.id>
2022-11-03 01:00:06 +0700

    CRUD categories

---
+++ app/api/v1/categories/model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { model, Schema } = mongoose;
+
+let categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+            minlength: [3, 'Name must be at least 3 characters'],
+            maxlength: [20, 'Name must be at most 20 characters'],
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = model('Category', categorySchema);
